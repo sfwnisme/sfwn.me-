3c592a496014767e90ee5722dbfb7c461988d484
Safwan <92028514+sfwnisme@users.noreply.github.com>
2022-09-19 13:31:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 let navIcon = document.getElementById("nav-icon");
 let nav = document.getElementById("nav");
+// let navLink = document.querySelectorAll("nav ul li a");
 let iconSpanOne = document.querySelector("#span-one");
 let iconSpanTwo = document.querySelector("#span-two");
 let overly = document.getElementById("overly");
-console.log(iconSpanTwo);
+// console.log(navLink);
 
-navIcon.addEventListener("click", function () {
+navIcon.addEventListener("click", function (e) {
   nav.classList.toggle("nav-active");
-  navIcon.classList.toggle("displayInitial");
+  this.classList.toggle("displayInitial");
   overly.classList.toggle("overly-active");
+  e.stopPropagation();
 });
+
+// This Solution Source Is => "https://www.youtube.com/watch?v=iP63fDXk-W8"
+document.onclick = function (e) {
+  if (
+    e.target.id !== "nav" &&
+    e.target.id !== "nav-icon" &&
+    e.target.id !== "overly"
+  ) {
+    nav.classList.remove("nav-active");
+    navIcon.classList.remove("displayInitial");
+    overly.classList.remove("overly-active");
+  }
+  e.stopPropagation();
+};
