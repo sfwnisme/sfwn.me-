e4df80ee2898d1a009dca76487814134db236016
Safwan <sfwnisme@gmail.com>
2022-09-27 14:04:12 +0300

    Replace The Footer From Html To main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 let navIcon = document.getElementById("nav-icon");
 let nav = document.getElementById("nav");
 // let navLink = document.querySelectorAll("nav ul li a");
@@ -9,21 +7,21 @@ let overly = document.getElementById("overly");
 // console.log(navLink);
 
 navIcon.addEventListener("click", function (e) {
-	nav.classList.toggle("nav-active");
-	this.classList.toggle("displayInitial");
-	overly.classList.toggle("overly-active");
-	e.stopPropagation();
+  nav.classList.toggle("nav-active");
+  this.classList.toggle("displayInitial");
+  overly.classList.toggle("overly-active");
+  e.stopPropagation();
 
-	// Remove The Navigation Bar If Click On Overly Or Links
-	// This Solution Source Is => "https://www.youtube.com/watch?v=iP63fDXk-W8"
-	document.onclick = function (e) {
-		if (e.target.id !== "nav" && e.target.id !== "nav-icon") {
-			nav.classList.remove("nav-active");
-			navIcon.classList.remove("displayInitial");
-			overly.classList.remove("overly-active");
-		}
-		e.stopPropagation();
-	};
+  // Remove The Navigation Bar If Click On Overly Or Links
+  // This Solution Source Is => "https://www.youtube.com/watch?v=iP63fDXk-W8"
+  document.onclick = function (e) {
+    if (e.target.id !== "nav" && e.target.id !== "nav-icon") {
+      nav.classList.remove("nav-active");
+      navIcon.classList.remove("displayInitial");
+      overly.classList.remove("overly-active");
+    }
+    e.stopPropagation();
+  };
 });
 
 /* ----------up Scroll */
@@ -31,20 +29,20 @@ navIcon.addEventListener("click", function (e) {
 let up = document.getElementById("up");
 
 up.onclick = () => {
-	scroll({
-		top: 0,
-		left: 0,
-		behavior: "smooth",
-	});
+  scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
 };
 
-window.onscroll = e => {
-	if (scrollY > 700) {
-		up.style.cssText = "opacity: 1; z-index: 1;";
-	} else {
-		up.style.cssText = "opacity: 0; z-index: -1;";
-		// e.preventDefault()
-	}
+window.onscroll = (e) => {
+  if (scrollY > 700) {
+    up.style.cssText = "opacity: 1; z-index: 1;";
+  } else {
+    up.style.cssText = "opacity: 0; z-index: -1;";
+    // e.preventDefault()
+  }
 };
 
 //
@@ -57,7 +55,30 @@ let workImageName = document.querySelectorAll(".works .work a");
 console.log(work);
 
 for (let i = 0; i < work.length; i++) {
-	work[i].title = work[i].innerText;
-	workImage[i].alt = workImageName[i].innerText;
-	workImageName[i].title = workImageName[i].innerText;
+  work[i].title = work[i].innerText;
+  workImage[i].alt = workImageName[i].innerText;
+  workImageName[i].title = workImageName[i].innerText;
 }
+
+/* --footer------------------- */
+
+let footer = document.createElement("footer");
+footer.className = "bg-ltest";
+let footerContainer = document.createElement("div");
+footerContainer.className = "container";
+
+let footerContent = document.createElement("h6");
+footerContent.className = "flex j-c-c align-items-c gap-05-rem";
+footerContent.innerText = "Elaborated By";
+
+let footerLink = document.createElement("a");
+footerLink.innerText = "Sfwn";
+footerContent.append(footerLink);
+
+let footerBox = document.createElement("div");
+footerBox.className = "box flex j-c-c align-items-c";
+footerBox.append(footerContent);
+
+footerContainer.appendChild(footerBox);
+footer.append(footerContainer);
+document.body.appendChild(footer);
