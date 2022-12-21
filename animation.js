0ca8055a5573852507fc79cd73f7b94af14912f5
sfwnisme <sfwnisme@gmail.com>
2022-12-21 07:26:05 +0300

    "

--- animation.js
+++ animation.js
@@ -1,15 +1,38 @@
 console.log("#".repeat(34));
 let docWidth = Math.max(document.body.offsetWidth);
 console.log(docWidth);
-let ser = document.querySelectorAll("[data-up=up]");
+let ser = document.querySelector("[data-up=up]");
 console.log(ser);
 
 window.onscroll = function () {
-  ser.forEach((e) => {
-    if (window.scrollY >= e.offsetTop - e.offsetHeight * 0.7) {
-      e.classList.add("show");
-    } else {
-      e.classList.add("hide");
-    }
-  });
+  if (window.scrollY >= ser.offsetTop - ser.offsetHeight * 0.5) {
+    ser.classList.remove("hide");
+    ser.classList.add("show");
+  } else {
+    ser.classList.remove("show");
+    ser.classList.add("hide");
+  }
 };
+
+function anim() {
+  console.log("#".repeat(34));
+  let docWidth = Math.max(document.body.offsetWidth);
+  console.log(docWidth);
+  let animation = document.querySelector(".work");
+  console.log(ser);
+
+  window.onscroll = function () {
+    if (window.scrollY >= animation.offsetTop - animation.offsetHeight * 0.5) {
+      animation.forEach((an) => {
+        an.classList.remove("hide");
+        an.classList.add("show");
+      });
+    } else {
+      animation.forEach((an) => {
+        an.classList.remove("show");
+        an.classList.add("hide");
+      });
+    }
+  };
+}
+anim();
