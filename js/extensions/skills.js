906f3e02473909340a6ecb7903f7a1a1e0c4f54a
Safwan <sfwnisme@gmail.com>
2022-12-27 10:54:53 +0300

    .

--- js/extensions/skills.js
+++ js/extensions/skills.js
@@ -4,7 +4,18 @@ export default function skills() {
   let calc = `${((100 / skills) * done).toFixed(2)}%`;
   console.log(calc);
 
+  // add the skills progress with animation
+  let section = document.querySelector("section");
+  let prog = document.querySelector(".skills-progress");
   let skillProgress = document.querySelector(".skills-progress");
-  console.log(skillProgress);
-  skillProgress.innerHTML = calc;
+  window.onscroll = function () {
+    if (scrollY > section.offsetTop - 300) {
+      console.log(skillProgress);
+      skillProgress.innerHTML = calc;
+      prog.setAttribute("style", `width: ${calc}`);
+    } else {
+      skillProgress.innerHTML = 0;
+      prog.setAttribute("style", `width: ${0}`);
+    }
+  };
 }
