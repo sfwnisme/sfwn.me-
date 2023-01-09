59f37edd9739206e1d00834315e677abb99b82a2
Safwan <sfwnisme@gmail.com>
2023-01-09 20:16:11 +0300

    .

--- js/extensions/preloader.js
+++ js/extensions/preloader.js
@@ -1,16 +1,9 @@
 export default function preloader() {
-  let time= 1500;
+  let time = 1000;
   let preloaderElement = document.querySelector(".preloader");
   document.documentElement.style.setProperty("--preloader-time", `${time}ms`);
   console.log(performance.now());
-  if (performance.now() < 1000) {
-    time = 5000;
-    console.log(time);
-  } else {
-    time = performance.now();
-    console.log(time);
-  }
-  setInterval(loader(), time);
+  setTimeout(loader(), time);
 
   function loader() {
     console.log("Preloader is working");
