3a6fa877b7d30161af60c1872320e8e91f1d1b3b
Safwan <sfwnisme@gmail.com>
2023-01-14 10:49:16 +0300

    .

--- js/extensions/detectDevice.js
+++ js/extensions/detectDevice.js
@@ -30,14 +30,14 @@ export default function detectDevice() {
 
     let alertIcon = document.createElement("div");
     alertIcon.className = "alert-icon";
-    // alertIcon.innerHTML = `
-    //   <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-versions" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-    //     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-    //     <rect x="10" y="5" width="10" height="14" rx="2"></rect>
-    //     <line x1="7" y1="7" x2="7" y2="17"></line>
-    //     <line x1="4" y1="8" x2="4" y2="16"></line>
-    //   </svg>`;
-    alertIcon.innerText = "attention";
+    alertIcon.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-versions" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+        <rect x="10" y="5" width="10" height="14" rx="2"></rect>
+        <line x1="7" y1="7" x2="7" y2="17"></line>
+        <line x1="4" y1="8" x2="4" y2="16"></line>
+      </svg>`;
+    // alertIcon.innerText = "attention";
     let x = document.createElement("div");
     let xIcon = `
       <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-x" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
