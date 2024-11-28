4622e44f0ffe5da9eb9be8e6e8daca65fdeb1814
Moamal-2000 <moamalalaapro1@gmail.com>
2024-11-28 14:57:37 +0300

    refactor: main.js code, reduce the code and remove unnessarily comments

--- js/main.js
+++ js/main.js
@@ -1,61 +1,46 @@
-/* .........skills......... */
-import skills from "./extensions/skills.js";
-skills();
-
-/* .........logo......... */
-import logo from "./extensions/logo.js";
-logo();
-
-/* .........github......... */
-import github from "./extensions/github.js";
-github();
-
-/* .........contact......... */
-// import contact from "./extensions/contact.js";
-// contact();
-
-/* .........footer......... */
-import footer from "./extensions/footer.js";
-footer();
-
-/* .........services......... */
-import services from "./extensions/services.js";
-services();
-
-// /* .........services......... */
-// import themee from "./extensions/theme.js";
-// themee();
-
-/* .........services......... */
-import preloader from "./extensions/preloader.js";
-preloader();
-
-/* .........services......... */
-import cursroFunction from "./extensions/cursor2.js";
-cursroFunction();
-
-/* .........services......... */
+import cursorFun from "./extensions/cursor2.js";
 import detectDevice from "./extensions/detectDevice.js";
-detectDevice();
-
-/* .........services......... */
-import navigation from "./extensions/nav.js";
-navigation();
-
-/* .........services......... */
 import emailFunction from "./extensions/email.js";
-emailFunction();
-
-/* .........services......... */
+import footer from "./extensions/footer.js";
+import github from "./extensions/github.js";
+import logo from "./extensions/logo.js";
+import navigation from "./extensions/nav.js";
+import preloader from "./extensions/preloader.js";
+import services from "./extensions/services.js";
+import skills from "./extensions/skills.js";
+// import contact from "./extensions/contact.js";
+// import theme from "./extensions/theme.js";
 // import lang from "./extensions/lang.js";
-// lang();
 
-/* .........AOS......... */
+const allAnchors = document.querySelectorAll("a");
+const cursor = document.querySelector(".cursor");
+const scrollerEle = document.querySelector(".scroller");
+const pageHeight =
+  document.documentElement.scrollHeight - document.documentElement.clientHeight;
+
+skills();
+logo();
+github();
+footer();
+services();
+cursorFun();
+detectDevice();
+preloader();
+navigation();
+emailFunction();
 AOS.init();
 
-//
-//
-//
+window.addEventListener("scroll", () => {
+  const scrollTop = document.documentElement.scrollTop;
+  scrollerEle.style.width = `${(scrollTop / pageHeight) * 100}%`;
+});
+
+allAnchors.forEach((a) => {
+  a.addEventListener("click", () => {
+    cursor.classList.add("active");
+  });
+});
+
 /* -----------Add The Name Of The Project To The Container Title */
 /* 
 let work = document.querySelectorAll(".works .work");
@@ -68,30 +53,3 @@ for (let i = 0; i < work.length; i++) {
   workImageName[i].title = workImageName[i].innerText;
 } 
 */
-
-let theme = document.querySelector("[data-theme = theme]");
-let svgs = document.querySelectorAll("svg");
-let body = document.querySelector("body");
-let mainColor = document.querySelectorAll(".main-color");
-
-/* ------------------------------------------------------------- */
-
-/* --Start Page Scroll Progress */
-let el = document.querySelector(".scroller");
-let height =
-  document.documentElement.scrollHeight - document.documentElement.clientHeight;
-
-window.addEventListener("scroll", () => {
-  const scrollTop = document.documentElement.scrollTop;
-  el.style.width = `${(scrollTop / height) * 100}%`;
-});
-/* --End Page Scroll Progress */
-
-let allA = document.querySelectorAll("a");
-let allBtn = document.querySelectorAll("button");
-let cursor = document.querySelector(".cursor");
-allA.forEach((a) => {
-  a.addEventListener("click", () => {
-    cursor.classList.add("active");
-  });
-});
